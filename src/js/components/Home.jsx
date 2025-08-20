87cf6555052d387b3719ede8c09e65d1b9383c54
gabrielafariac <126210298+gabrielafariac@users.noreply.github.com>
2025-08-20 22:15:27 +0000

    first

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [chores, setChores] = useState(["Vaccum", "clean","buy cat food", "Dust"])
+	const [newchore,setNewChore] = useState([""])
+	const nextChore =()=>{
+		setChores([newchore,...chores])
+		setNewChore("")
+	}
+
+
+		
+	
+	const deleteChore = (item) =>{
+
+		const filteredChores = chores.filter(
+			(choredata) => choredata != item
+		)
+		console.log("dust: ", filteredChores)
+		setChores(filteredChores)
+
+	}
 	return (
 		<div className="text-center">
-            
+			<h1> To Do List</h1>
+			<input 
+			value={newchore}
+			className=""
+			type="text"
+			placeholder="Add new task"
+			onChange= {(e) =>{
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+				const newTask = e.target.value
+				setNewChore(newTask)
+			}
+			}
+			onKeyDown={(e)=>{
+				if (e.key == "Enter"){
+					nextChore(newchore)
+					}
+				}		
+			}
+			/>
+			<button onClick={() => nextChore()}>
+				Add Task
+			</button>
+			<ul>
+				{chores.map((item, i) => {
+				return (
+					<div>
+						<li key={i + "chores"}>{item}</li>	
+					<span
+					 style={{ cursor: "pointer", marginLeft: "8px", color: "red" }}
+					onClick={()=> {
+						deleteChore(item)
+					}
+				}
+					> X </span>
+
+				
+				</div>
+				)}
+			
+			)}	
+			</ul>
+
 		</div>
+	
 	);
 };
 
